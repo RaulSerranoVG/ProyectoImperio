0ff5266b918f40da6fadc2e235adc4b21a73ac1e
Raulse <raulse01@ucm.es>
2018-03-17 20:36:32 +0100

    Implementada funcionalidad de Drag & Drop

---
+++ Assets/Proyecto Imperio/Scripts/Draggable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;//Vamos a implementar interfaces para que sea Draggable
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public Transform parentToReturnTo = null;
+
+    public enum Slot { WEAPON,CHEST,HEAD,INVENTORY};
+    public Slot typeOfItem = Slot.WEAPON;
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        Debug.Log("OnBeginDrag");
+
+
+        parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnDrag");
+
+        //La posición de la carta es la posición del cursor
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+
+        this.transform.SetParent(parentToReturnTo);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+    }
+
+}
---
+++ Assets/Proyecto Imperio/Scripts/DropZone.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;//Vamos a implementar interfaces para soltar cartas
+
+public class DropZone : MonoBehaviour ,IDropHandler,IPointerEnterHandler, IPointerExitHandler{
+
+    public Draggable.Slot typeOfItem = Draggable.Slot.INVENTORY;
+
+    //Es llamado cuando el puntero del ratón entra en el GameObject
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerEnter");
+
+    }
+
+    //Es llamado cuando el puntero del ratón sale del GameObject
+    public void OnPointerExit(PointerEventData eventData)
+    {
+       // Debug.Log("OnPointerExit");
+
+    }
+
+    //Es llamado cuando se suelta un objeto en el GameObject
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            if (typeOfItem == d.typeOfItem || typeOfItem == Draggable.Slot.INVENTORY)
+                d.parentToReturnTo = this.transform;
+
+        }
+    }
+
+
+}
