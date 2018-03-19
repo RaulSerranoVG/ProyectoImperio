57e30abc436f82be6b01d66923e7e349ed54d02e
Raulse <raulse01@ucm.es>
2018-03-19 11:39:49 +0100

    Tutorial Drag&Drop hecho y comentado

--- Assets/Proyecto Imperio/Scripts/Draggable.cs
+++ Assets/Proyecto Imperio/Scripts/Draggable.cs
@@ -1,40 +1,121 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;//Vamos a implementar interfaces para que sea Draggable
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
+    //Siempre guardamos de que zona viene para poder retornarla
     public Transform parentToReturnTo = null;
 
-    public enum Slot { WEAPON,CHEST,HEAD,INVENTORY};
-    public Slot typeOfItem = Slot.WEAPON;
+    //Guardamos el padre del placeholder para poder retonar la carta
+    public Transform placeholderParent = null;
 
+    //Para mostrar un hueco vacío entre las cartas
+    GameObject placeholder = null;
+
+
+    //Si necesitamos información de que tipo es la carta
+    /*public enum Slot { WEAPON,CHEST,HEAD,INVENTORY};
+    public Slot typeOfItem = Slot.WEAPON;
+    */
+
+
+    //Método que es llamado una vez cuando cogemos una carta con el ratón
     public void OnBeginDrag(PointerEventData eventData) {
         Debug.Log("OnBeginDrag");
 
+        //Creamos el hueco vacío
+        CreatePlaceholder();
 
+        //Actualizamos de donde viene
         parentToReturnTo = this.transform.parent;
+        placeholderParent = parentToReturnTo;
+
+        //Eliminamos su parentesco a la zona correspondiente
         this.transform.SetParent(this.transform.parent.parent);
 
+        //Bloqueamos raycast de la carta para poder dropearla en una zona
         GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+
+        //Podemos iterar en todas las zonas disponibles en el juego
+        //DropZone[] zones = GameObject.FindObjectsOfType<DropZone>();
     }
 
+    //Método que es llamado continuamente cuando arrastramos una carta con el ratón
     public void OnDrag(PointerEventData eventData)
     {
         //Debug.Log("OnDrag");
 
         //La posición de la carta es la posición del cursor
         this.transform.position = eventData.position;
+
+        //Movemos el hueco vacío a donde le toque
+        UpdatePlaceholder();
     }
 
+    //Método que es llamado una vez cuando soltamos el clickIzquierdo del raton (Después de OnDrop())
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("OnEndDrag");
 
+        //Metemos la carta en una zona, ya sea de donde venia o con el Parent actualizado en otra zona
         this.transform.SetParent(parentToReturnTo);
+        this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex()); //La metemos en el hueco correspondiente
+
+        //Puede volver a ser cogida
         GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        //Destruimos el hueco vacío
+        Destroy(placeholder);
+
+        //EventSystem.current.RaycastAll(eventData);
+    }
+
+    //Crea el GameObject en la zona correspondiente para mostrar un hueco vacío
+    void CreatePlaceholder()
+    {
+        placeholder = new GameObject();
+
+        //Lo ponemos en la posición correspondiente de la zona
+        placeholder.transform.SetParent(this.transform.parent);
+        placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+
+        //Queremos que ocupe lo mismo que la carta en el layout
+        LayoutElement le = placeholder.AddComponent<LayoutElement>();
+        le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+        le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+        le.flexibleWidth = 0;
+        le.flexibleHeight = 0;
+
 
     }
 
+    //Actualiza la posición del hueco vacío
+    void UpdatePlaceholder()
+    {
+        //Actualizamos el parentesco (Cambia cuando entramos en otra zona)
+        if (placeholder.transform.parent != placeholderParent)
+            placeholder.transform.SetParent(placeholderParent);
+
+        //Movemos el hueco vacío a donde le corresponda del layour
+        int newSiblingIndex = placeholderParent.childCount;
+
+        for (int i = 0; i < placeholderParent.childCount; i++)
+        {
+            if (this.transform.position.x < placeholderParent.GetChild(i).position.x)
+            {
+                newSiblingIndex = i;
+                if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+                    newSiblingIndex--;
+                break;
+            }
+
+        }
+
+        placeholder.transform.SetSiblingIndex(newSiblingIndex);
+
+    }
 }
--- Assets/Proyecto Imperio/Scripts/DropZone.cs
+++ Assets/Proyecto Imperio/Scripts/DropZone.cs
@@ -5,23 +5,42 @@ using UnityEngine.EventSystems;//Vamos a implementar interfaces para soltar cart
 
 public class DropZone : MonoBehaviour ,IDropHandler,IPointerEnterHandler, IPointerExitHandler{
 
-    public Draggable.Slot typeOfItem = Draggable.Slot.INVENTORY;
+    //Si necesitamos distinguir entre zonas
+    //public Draggable.Slot typeOfItem = Draggable.Slot.INVENTORY;
 
     //Es llamado cuando el puntero del ratón entra en el GameObject
     public void OnPointerEnter(PointerEventData eventData)
     {
         //Debug.Log("OnPointerEnter");
 
+        //Detectamos si llevamos una carta
+        if (eventData.pointerDrag != null)
+        {
+            Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+            if (d != null)
+                //Establecemos padre del placeholder objeto a esta nueva zona
+                d.placeholderParent = this.transform;
+            
+        }
     }
 
     //Es llamado cuando el puntero del ratón sale del GameObject
     public void OnPointerExit(PointerEventData eventData)
     {
-       // Debug.Log("OnPointerExit");
+        // Debug.Log("OnPointerExit");
 
+        //Detectamos si llevamos una carta
+        if (eventData.pointerDrag != null)
+        {
+            Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+            if (d != null && d.placeholderParent == this.transform)
+                //Establecemos el padre del placeholder del objeto al padre que tenia que retornar
+                d.placeholderParent = d.parentToReturnTo;
+            
+        }
     }
 
-    //Es llamado cuando se suelta un objeto en el GameObject
+    //Es llamado cuando se suelta un objeto en el GameObject (Antes que OnEndDrag())
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
@@ -29,8 +48,10 @@ public class DropZone : MonoBehaviour ,IDropHandler,IPointerEnterHandler, IPoint
         Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
         if (d != null)
         {
-            if (typeOfItem == d.typeOfItem || typeOfItem == Draggable.Slot.INVENTORY)
-                d.parentToReturnTo = this.transform;
+            //Si tenemos que distinguir que tipo de carta es
+            //if (typeOfItem == d.typeOfItem || typeOfItem == Draggable.Slot.INVENTORY)
+
+            d.parentToReturnTo = this.transform;//Establecemos el nuevo padre del objeto dropeado
 
         }
     }
